9d844e37e98a0656f767f6cefca4d217668392f7
Michael Ragsdale <hireme@michaelpragsdale.com>
2025-08-21 20:46:57 -0400

    Trying something with the seeder,. try 4

--- resources/views/layouts/partials/navigation/_button-controls.blade.php
+++ resources/views/layouts/partials/navigation/_button-controls.blade.php
@@ -1,10 +1,44 @@
-div class="button-group">
-    @foreach ($nav_items as $url => $button)
-        <a href="{{ url($url) }}" class="button button-outline-secondary">
-            @if (!empty($button['icon']))
-                <i class="fa-duotone fa-fw fa-{{ $button['icon'] }}"></i>
-            @endif
-            <span>{{ $button['text'] }}</span>
-        </a>
-    @endforeach
-</div>
+@auth
+    <!-- Logged in user dropdown -->
+    <div class="hidden sm:flex sm:items-center sm:ml-6">
+        <x-dropdown align="right" width="48">
+            <x-slot name="trigger">
+                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                    {{-- Use optional() helper to prevent errors on stale sessions --}}
+                    <div>{{ optional(Auth::user())->name }}</div>
+
+                    <div class="ml-1">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </button>
+            </x-slot>
+
+            <x-slot name="content">
+                <x-dropdown-link :href="route('admin.dashboard')">
+                    {{ __('Admin Dashboard') }}
+                </x-dropdown-link>
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
+            </x-slot>
+        </x-dropdown>
+    </div>
+@else
+    <!-- Guest links -->
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+    @if (Route::has('register'))
+        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+    @endif
+@endauth
+
