b66df9c4490e0003bd635e14449e10b83b88d437
Michael Ragsdale <hireme@michaelpragsdale.com>
2025-08-21 21:56:49 -0400

    Still trying to fix the Projects, again x15 - this is getting out of hand

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $page_title ?? "Michael Ragsdale's Portfolio" }}</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <script src="https://kit.fontawesome.com/ec060982d4.js" crossorigin="anonymous"></script>
+        <title>{{ $page_title ?? config('app.name', 'Laravel') }}</title>
 
-    @vite(['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/main.js'])
-</head>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts and Styles -->
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
     <body class="font-sans antialiased {{ $body_class ?? '' }}">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
 
             {{-- This includes the main site header --}}
             @include('layouts.partials._header')
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
+            <main class="site-content" id="content">
+                <div class="site-body-wrapper">
+                    @if(isset($sidebar) && $sidebar)
+                        @include('layouts.partials.' . $sidebar)
+                    @endif
+
+                    <div class="main-content">
+                        {{ $slot }}
+                    </div>
+                </div>
             </main>
 
             {{-- This includes the main site footer --}}
