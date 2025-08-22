de3bb6b0ed8e5d3c5560e4ee2a49a83011e8eab2
Michael Ragsdale <hireme@michaelpragsdale.com>
2025-08-21 21:48:26 -0400

    Still trying to fix the Projects, again x11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $page_title ?? "Michael Ragsdale's Portfolio" }}</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <script src="https://kit.fontawesome.com/ec060982d4.js" crossorigin="anonymous"></script>
+        <title>{{ $page_title ?? config('app.name', 'Laravel') }}</title>
 
-    @vite(['resources/scss/app.scss', 'resources/js/app.js', 'resources/js/main.js'])
-</head>
-<body class="{{ $body_class ?? '' }}" data-page-script="{{ $page_script ?? '' }}">
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <a href="#content" class="skip-link">Skip to Main Content</a>
+        <!-- Scripts and Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased {{ $body_class ?? '' }}">
+        <div class="min-h-screen bg-gray-100">
 
-    <div class="page-container">
+            {{-- This includes the main site header --}}
+            @include('layouts.partials._header')
 
-        @include('layouts.partials._header')
-
-        <div class="site-body-wrapper">
-            @if (!empty($sidebar))
-                @include('layouts.partials.' . $sidebar)
-            @endif
-            <main class="site-content" id="content">
-                @yield('content')
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
             </main>
+
+            {{-- This includes the main site footer --}}
+            @include('layouts.partials._footer')
         </div>
-
-        @include('layouts.partials._footer')
-
-    </div>
-
-</body>
+    </body>
 </html>
