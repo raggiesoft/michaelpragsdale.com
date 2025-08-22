ffeb3aadc021baefdb28d03b2300305f4a3455df
Michael Ragsdale <hireme@michaelpragsdale.com>
2025-08-21 20:36:04 -0400

    Trying something with the seeder,. try 3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Michael Ragsdale',
-            'email' => 'hireme@michaelpragsdale.com',
-        ]);
+        // Use firstOrCreate to prevent duplicate entry errors.
+        // It will find the user by email or create them if they don't exist.
+        User::firstOrCreate(
+            ['email' => 'hireme@michaelpragsdale.com'],
+            [
+                'name' => 'Michael Ragsdale',
+                'password' => Hash::make('AXBrkQlZMmFY137'), // A default password is required for creation
+            ]
+        );
 
         // Call your new ProjectSeeder
         $this->call([
